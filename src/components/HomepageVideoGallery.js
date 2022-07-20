4b38e4d2d2a00130e7ff84b22c913b54d025dc76
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2022-07-20 19:44:52 +0100

    Create 'creactVideoGalleryItems.js' script

--- src/components/HomepageVideoGallery.js
+++ src/components/HomepageVideoGallery.js
@@ -1,77 +1,13 @@
 import { useState, useEffect } from "react";
-import { abbreviateNumber } from "js-abbreviation-number";
-import moment from "moment";
-import getVideosDataWithChannelData from "../getVideosDataWithChannelData";
-import convertYouTubeAPIDuration from "../convertYouTubeAPIDuration";
 import Loader from "./Loader";
+import getVideosDataWithChannelData from "../getVideosDataWithChannelData";
+import creactVideoGalleryItems from "../creactVideoGalleryItems";
 import "../styles/HomepageVideoGallery.css";
 
 function HomepageVideoGallery() {
   const [videosDataWithChannelData, setVideosDataWithChannelData] =
     useState(null);
-
-  let imgElementArray = null;
-
-  if (videosDataWithChannelData) {
-    imgElementArray = videosDataWithChannelData.map(function (item) {
-      const publishedDate = item.publishedAt.slice(0, 10);
-      const publishedDateWithoutHyphens = publishedDate.replace(/-/g, "");
-      const relativeTime = moment(
-        publishedDateWithoutHyphens,
-        "YYYYMMDD"
-      ).fromNow();
-
-      function handleClick(e) {
-        e.currentTarget.style.backgroundColor = "#dedede";
-        window.location.href = `https://www.youtube.com/watch?v=${item.id}`;
-      }
-
-      return (
-        <div className="video-gallery-item" key={item.id} onClick={handleClick}>
-          <div className="video-gallery-item-image-div">
-            <img
-              alt={item.title}
-              src={item.thumbnails.medium.url}
-              className="video-gallery-item-image"
-            />
-            <span className="video-gallery-item-image-duration">
-              {convertYouTubeAPIDuration(item.contentDetails.duration)}
-            </span>
-          </div>
-          <div className="video-gallery-item-image-info-div">
-            <span>
-              <a
-                href={`https://www.youtube.com/channel/${item.channelId}`}
-                title={item.channelTitle}
-              >
-                <img
-                  alt=""
-                  src={item.defaultChannelAvatar}
-                  className="video-gallery-item-avatar-image"
-                />
-              </a>
-            </span>
-            <span className="video-gallery-item-image-info-span">
-              <h3 title={item.title}>{item.title}</h3>
-              <div>
-                <a
-                  href={`https://www.youtube.com/channel/${item.channelId}`}
-                  title={item.channelTitle}
-                  className="channel-link"
-                >
-                  {item.channelTitle}
-                </a>
-              </div>
-              <div className="video-view-count">
-                <span>{`${abbreviateNumber(item.viewCount, 0)} views`} • </span>
-                <span>{relativeTime}</span>
-              </div>
-            </span>
-          </div>
-        </div>
-      );
-    });
-  }
+  const [videoGalleryItems, setVideoGalleryItems] = useState(null);
 
   useEffect(() => {
     getVideosDataWithChannelData().then((items) =>
@@ -79,9 +15,13 @@ function HomepageVideoGallery() {
     );
   }, []);
 
+  useEffect(() => {
+    setVideoGalleryItems(creactVideoGalleryItems(videosDataWithChannelData));
+  }, [videosDataWithChannelData]);
+
   return (
     <div id="homepage-video-gallery" className="if-wide-sidebar-is-active">
-      {imgElementArray ? imgElementArray : Loader()}
+      {videoGalleryItems ? videoGalleryItems : Loader()}
     </div>
   );
 }
---
+++ src/creactVideoGalleryItems.js
@@ -0,0 +1,72 @@
+import { abbreviateNumber } from "js-abbreviation-number";
+import moment from "moment";
+import convertYouTubeAPIDuration from "./convertYouTubeAPIDuration";
+
+function creactVideoGalleryItems(videosDataWithChannelData) {
+  let videoGalleryItemsArray = null;
+
+  if (videosDataWithChannelData) {
+    videoGalleryItemsArray = videosDataWithChannelData.map(function (item) {
+      const publishedDate = item.publishedAt.slice(0, 10);
+      const publishedDateWithoutHyphens = publishedDate.replace(/-/g, "");
+      const relativeTime = moment(
+        publishedDateWithoutHyphens,
+        "YYYYMMDD"
+      ).fromNow();
+
+      function handleClick(e) {
+        e.currentTarget.style.backgroundColor = "#dedede";
+        window.location.href = `https://www.youtube.com/watch?v=${item.id}`;
+      }
+
+      return (
+        <div className="video-gallery-item" key={item.id} onClick={handleClick}>
+          <div className="video-gallery-item-image-div">
+            <img
+              alt={item.title}
+              src={item.thumbnails.medium.url}
+              className="video-gallery-item-image"
+            />
+            <span className="video-gallery-item-image-duration">
+              {convertYouTubeAPIDuration(item.contentDetails.duration)}
+            </span>
+          </div>
+          <div className="video-gallery-item-image-info-div">
+            <span>
+              <a
+                href={`https://www.youtube.com/channel/${item.channelId}`}
+                title={item.channelTitle}
+              >
+                <img
+                  alt=""
+                  src={item.defaultChannelAvatar}
+                  className="video-gallery-item-avatar-image"
+                />
+              </a>
+            </span>
+            <span className="video-gallery-item-image-info-span">
+              <h3 title={item.title}>{item.title}</h3>
+              <div>
+                <a
+                  href={`https://www.youtube.com/channel/${item.channelId}`}
+                  title={item.channelTitle}
+                  className="channel-link"
+                >
+                  {item.channelTitle}
+                </a>
+              </div>
+              <div className="video-view-count">
+                <span>{`${abbreviateNumber(item.viewCount, 0)} views`} • </span>
+                <span>{relativeTime}</span>
+              </div>
+            </span>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  return videoGalleryItemsArray;
+}
+
+export default creactVideoGalleryItems;
